770dc0a88434e6ad8ebacdd77e0cfb035f6831c1
John <john200_@hotmail.com>
2020-10-23 11:34:15 -0600

    Se cambio los nombres de las Views, se agrego una nueva View Expediente, en cada View se agrego un tabla, se instalo una libreria para iconos

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,5 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      app
-      v-model="drawer"
-      :clipped="$vuetify.breakpoint.mdAndUp"
-    >
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-app-bar
       app
       color="purple"
@@ -41,33 +10,64 @@
     >
       <v-toolbar-title>
         <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-        <span>My files</span>
+        <span>Professionals</span>
       </v-toolbar-title>
     </v-app-bar>
+
+    <v-navigation-drawer
+      :clipped="$vuetify.breakpoint.mdAndUp"
+      app
+      v-model="drawer"
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            MedAssitance
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider dark light></v-divider>
+
+      <v-list nav dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="{ name: item.screen }"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <router-view />
+        </v-slide-y-transition>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
-    drawer: false,
+    drawer: true,
     items: [
-      { title: "Dashboard", icon: "mdi-view-dashboard" },
-      { title: "Photos", icon: "mdi-image" },
-      { title: "About", icon: "mdi-help-box" },
-    ],
-    right: null,
-    admins: [
-      ["Management", "mdi-account-multiple-outline"],
-      ["Settings", "mdi-cog-outline"],
+      { title: "Usuarios", icon: "fas fa-users", screen: "Usuarios" },
+      { title: "Expediente", icon: "far fa-address-card", screen: "Expediente" },
+      { title: "Medicos", icon: "fas fa-user-md", screen: "Medicos" },
     ],
   }),
 
